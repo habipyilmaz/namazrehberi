d7c1048d608489e20e0ac85ba924b661ef30ff6d
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-12 00:11:32 +0300

    çoklu

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,21 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div>
+      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
+       <h1>SAV</h1>
+      <h1>YA NEBİ SELAM ALEYKE</h1>      
        <h1>SAV</h1>
       <h1>YA NEBİ SELAM ALEYKE</h1>
     </div>
